c96591adecb0e081d4cd5d87b9e033acf9e1e670
haohaosong <742398269@qq.com>
2016-12-05 09:31:16 +0800

    Select window and villager

---
+++ UISelect.java
@@ -0,0 +1,68 @@
+
+/*
+* author:haohaosong
+* date:2016/12/5
+* note:The UISelect window when the admin want to select the information
+*/
+
+import java.awt.*;  
+import java.awt.event.*;    
+import javax.swing.*;  
+  
+public class UISelect extends JFrame implements ActionListener  
+{   
+         //定义组件  
+        JButton jb1,jb2,jb3,jb4=null;  
+        JPanel jp1,jp2,jp3,jp4=null;  
+        JLabel jlb1,jlb3,jlb4=null;  
+          
+        public static void main(String[] args) {  
+            // TODO Auto-generated method stub  
+        	UISelect  ui=new UISelect();  
+        }  
+          
+        //构造函数  
+        public  UISelect()//构造函数
+        {  
+            //创建组件  
+            jb1=new JButton("按姓名查询");  
+            jb2=new JButton("按职务查询");
+            jb3=new JButton("按身份证号"); 
+            //jb4=new JButton("按性别查询"); 
+
+            jp1=new JPanel();  
+            jp2=new JPanel();  
+            jp3=new JPanel();                
+            jp4=new JPanel();
+            
+            jlb1=new JLabel("您好，现在是信息查询界面");    
+            jlb3=new JLabel("全村公告：");  
+            jlb4=new JLabel("村长选举大会正在召开");  
+              
+            jp1.add(jlb1);  
+            jp2.add(jb1);  
+            jp2.add(jb2);  
+            jp2.add(jb3);
+            //jp2.add(jb4);
+            
+            jp3.add(jlb3);  
+            jp3.add(jlb4);
+                       
+            this.add(jp1);  
+            this.add(jp2);  
+            this.add(jp3);      
+              
+            //设置布局管理器  
+            this.setLayout(new GridLayout(5,5,50,50));  
+            this.setTitle("村民信息管理系统");  
+            this.setSize(600,450);  
+            this.setLocation(200, 200);       
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+            this.setVisible(true);  
+}  
+        @Override  
+        public void actionPerformed(ActionEvent e) {  
+            // TODO Auto-generated method stub  
+              
+        }  
+}  
---
+++ Villager.java
@@ -0,0 +1,44 @@
+
+public class Villager {
+	private String name;
+	private  int age;
+	private String ID;
+	private String post;
+	private String address;
+	private String nation;
+	private String education;
+	Villager(String name,int age,String ID,String post,String address,String nation,String education)
+	{
+		this.name = name;
+		this.age = age;
+		this.ID = ID;
+		this.post = post;
+		this.address = address;
+		this.nation = nation;
+		this.education = education;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	public String getID()
+	{
+		return ID;
+	}
+	public String getAddress()
+	{
+		return address;
+	}
+	public String getNation()
+	{
+		return nation;
+	}
+	public String getEducation()
+	{
+		return education;
+	}
+}
